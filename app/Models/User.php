2d0b6ea4fe78952ddb419a22926a142275e8ba59
rebeckaelin <rebeckaelin@hotmail.com>
2025-01-09 10:33:14 +0100

    added code to create, update and delete chirps

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
+    }
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
